36231c4248019099a5dc38cc6bfcc578c9d4cb57
isaiahnoelsalazar <isaiahnoelsalazar474@gmail.com>
2026-01-11 14:13:08 +0800

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace NETSimpleFunctions
+{
+    public class Class1
+    {
+        public static void TestMethod()
+        {
+            Console.WriteLine("This is a test method.");
+        }
+    }
+}
