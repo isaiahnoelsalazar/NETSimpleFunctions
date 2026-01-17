aeb4a47d13de48434234122d7027fdb7372068c3
isaiahnoelsalazar <isaiahnoelsalazar474@gmail.com>
2026-01-17 17:50:25 +0800

    update: renamed a few variables

--- PyCS.cs
+++ PyCS.cs
@@ -74,19 +74,19 @@ namespace NETSimpleFunctions
                     try
                     {
                         Directory.CreateDirectory("python3_13");
-                        string zipPath = "python-3.13.5-embed-amd64.zip";
-                        string extractPath = "python3_13";
-                        ZipFile.ExtractToDirectory(zipPath, extractPath);
+                        string PythonZipPath = "python-3.13.5-embed-amd64.zip";
+                        string PythonZipExtractPath = "python3_13";
+                        ZipFile.ExtractToDirectory(PythonZipPath, PythonZipExtractPath);
 
                         using (FileStream fs = File.OpenWrite("python3_13\\python313._pth"))
                         {
-                            string toWrite = "python313.zip\r\n.\r\n\r\n# Uncomment to run site.main() automatically\r\nimport site\r\n";
-                            fs.Write(Encoding.UTF8.GetBytes(toWrite), 0, Encoding.UTF8.GetBytes(toWrite).Length);
+                            string ToWrite = "python313.zip\r\n.\r\n\r\n# Uncomment to run site.main() automatically\r\nimport site\r\n";
+                            fs.Write(Encoding.UTF8.GetBytes(ToWrite), 0, Encoding.UTF8.GetBytes(ToWrite).Length);
                         }
 
-                        string zipPath1 = "python3_13\\python313.zip";
-                        string extractPath1 = "python3_13\\python313";
-                        ZipFile.ExtractToDirectory(zipPath1, extractPath1);
+                        string PythonZipPath1 = "python3_13\\python313.zip";
+                        string PythonZipExtractPath1 = "python3_13\\python313";
+                        ZipFile.ExtractToDirectory(PythonZipPath1, PythonZipExtractPath1);
 
                         SimpleFileHandler.ProjectToLocation(Assembly.GetExecutingAssembly(), "sitecustomize.py", "python3_13");
                     }
@@ -118,13 +118,13 @@ namespace NETSimpleFunctions
                     {
                         System.Console.WriteLine("Downloading get-pip...");
                     }
-                    var webReq = (HttpWebRequest)HttpWebRequest.Create("https://bootstrap.pypa.io/get-pip.py");
-                    var res = webReq.GetResponse();
-                    var content = res.GetResponseStream();
+                    var WebRequest = (HttpWebRequest)HttpWebRequest.Create("https://bootstrap.pypa.io/get-pip.py");
+                    var Response = WebRequest.GetResponse();
+                    var Content = Response.GetResponseStream();
 
-                    using (var fileStream = File.Create("python3_13\\get-pip.py"))
+                    using (FileStream fs = File.Create("python3_13\\get-pip.py"))
                     {
-                        content.CopyTo(fileStream);
+                        Content.CopyTo(fs);
                     }
                 }
                 else
@@ -170,13 +170,13 @@ namespace NETSimpleFunctions
                     }
                     try
                     {
-                        ProcessStartInfo run0 = new ProcessStartInfo();
-                        run0.FileName = "python3_13\\python.exe";
-                        run0.Arguments = "python3_13\\get-pip.py";
-                        run0.UseShellExecute = false;
-                        run0.RedirectStandardOutput = true;
-                        run0.CreateNoWindow = true;
-                        Process = Process.Start(run0);
+                        ProcessStartInfo PIS = new ProcessStartInfo();
+                        PIS.FileName = "python3_13\\python.exe";
+                        PIS.Arguments = "python3_13\\get-pip.py";
+                        PIS.UseShellExecute = false;
+                        PIS.RedirectStandardOutput = true;
+                        PIS.CreateNoWindow = true;
+                        Process = Process.Start(PIS);
                         StreamReader reader = Process.StandardOutput;
                         if (reader.ReadToEnd().Length != 0)
                         {
@@ -204,13 +204,13 @@ namespace NETSimpleFunctions
 
         public void Pip(string[] args)
         {
-            ProcessStartInfo run0 = new ProcessStartInfo();
-            run0.FileName = "python3_13\\Scripts\\pip.exe";
-            run0.Arguments = "install " + string.Join(" ", args);
-            run0.UseShellExecute = false;
-            run0.RedirectStandardOutput = true;
-            run0.CreateNoWindow = Console;
-            Process = Process.Start(run0);
+            ProcessStartInfo PIS = new ProcessStartInfo();
+            PIS.FileName = "python3_13\\Scripts\\pip.exe";
+            PIS.Arguments = "install " + string.Join(" ", args);
+            PIS.UseShellExecute = false;
+            PIS.RedirectStandardOutput = true;
+            PIS.CreateNoWindow = Console;
+            Process = Process.Start(PIS);
             StreamReader reader = Process.StandardOutput;
             if (Console)
             {
@@ -220,13 +220,13 @@ namespace NETSimpleFunctions
 
         public void PipUpgrade(string[] args)
         {
-            ProcessStartInfo run0 = new ProcessStartInfo();
-            run0.FileName = "python3_13\\Scripts\\pip.exe";
-            run0.Arguments = "install --upgrade " + string.Join(" ", args);
-            run0.UseShellExecute = false;
-            run0.RedirectStandardOutput = true;
-            run0.CreateNoWindow = Console;
-            Process = Process.Start(run0);
+            ProcessStartInfo PIS = new ProcessStartInfo();
+            PIS.FileName = "python3_13\\Scripts\\pip.exe";
+            PIS.Arguments = "install --upgrade " + string.Join(" ", args);
+            PIS.UseShellExecute = false;
+            PIS.RedirectStandardOutput = true;
+            PIS.CreateNoWindow = Console;
+            Process = Process.Start(PIS);
             StreamReader reader = Process.StandardOutput;
             if (Console)
             {
@@ -236,13 +236,13 @@ namespace NETSimpleFunctions
 
         public void PipLocal(string[] args)
         {
-            ProcessStartInfo run0 = new ProcessStartInfo();
-            run0.FileName = "python3_13\\Scripts\\pip.exe";
-            run0.Arguments = "install " + string.Join(" ", args) + " --no-index --find-links /";
-            run0.UseShellExecute = false;
-            run0.RedirectStandardOutput = true;
-            run0.CreateNoWindow = Console;
-            Process = Process.Start(run0);
+            ProcessStartInfo PIS = new ProcessStartInfo();
+            PIS.FileName = "python3_13\\Scripts\\pip.exe";
+            PIS.Arguments = "install " + string.Join(" ", args) + " --no-index --find-links /";
+            PIS.UseShellExecute = false;
+            PIS.RedirectStandardOutput = true;
+            PIS.CreateNoWindow = Console;
+            Process = Process.Start(PIS);
             StreamReader reader = Process.StandardOutput;
             if (Console)
             {
@@ -269,28 +269,28 @@ namespace NETSimpleFunctions
             File.Create("python3_13\\main.py").Close();
             File.WriteAllText("python3_13\\main.py", script);
 
-            ProcessStartInfo run0 = new ProcessStartInfo();
-            run0.FileName = "python3_13\\python.exe";
-            run0.Arguments = "python3_13\\main.py";
-            run0.UseShellExecute = false;
-            run0.RedirectStandardOutput = true;
-            run0.CreateNoWindow = Console;
-            Process = Process.Start(run0);
+            ProcessStartInfo PIS = new ProcessStartInfo();
+            PIS.FileName = "python3_13\\python.exe";
+            PIS.Arguments = "python3_13\\main.py";
+            PIS.UseShellExecute = false;
+            PIS.RedirectStandardOutput = true;
+            PIS.CreateNoWindow = Console;
+            Process = Process.Start(PIS);
             StreamReader reader = Process.StandardOutput;
             System.Console.WriteLine(reader.ReadToEnd());
         }
 
         public void RunFile(string filePath)
         {
-            ProcessStartInfo run0 = new ProcessStartInfo();
-            run0.FileName = "python3_13\\python.exe";
-            run0.Arguments = filePath;
-            run0.UseShellExecute = false;
-            run0.RedirectStandardOutput = true;
-            run0.CreateNoWindow = Console;
-            Process = Process.Start(run0);
-            StreamReader reader = Process.StandardOutput;
-            System.Console.WriteLine(reader.ReadToEnd());
+            ProcessStartInfo PIS = new ProcessStartInfo();
+            PIS.FileName = "python3_13\\python.exe";
+            PIS.Arguments = filePath;
+            PIS.UseShellExecute = false;
+            PIS.RedirectStandardOutput = true;
+            PIS.CreateNoWindow = Console;
+            Process = Process.Start(PIS);
+            StreamReader sr = Process.StandardOutput;
+            System.Console.WriteLine(sr.ReadToEnd());
         }
 
         public string GetOutput(string script)
@@ -298,28 +298,28 @@ namespace NETSimpleFunctions
             File.Create("python3_13\\main.py").Close();
             File.WriteAllText("python3_13\\main.py", script);
 
-            ProcessStartInfo run0 = new ProcessStartInfo();
-            run0.FileName = "python3_13\\python.exe";
-            run0.Arguments = "python3_13\\main.py";
-            run0.UseShellExecute = false;
-            run0.RedirectStandardOutput = true;
-            run0.CreateNoWindow = Console;
-            Process = Process.Start(run0);
-            StreamReader reader = Process.StandardOutput;
-            return reader.ReadToEnd();
+            ProcessStartInfo PIS = new ProcessStartInfo();
+            PIS.FileName = "python3_13\\python.exe";
+            PIS.Arguments = "python3_13\\main.py";
+            PIS.UseShellExecute = false;
+            PIS.RedirectStandardOutput = true;
+            PIS.CreateNoWindow = Console;
+            Process = Process.Start(PIS);
+            StreamReader sr = Process.StandardOutput;
+            return sr.ReadToEnd();
         }
 
         public string GetFileOutput(string filePath)
         {
-            ProcessStartInfo run0 = new ProcessStartInfo();
-            run0.FileName = "python3_13\\python.exe";
-            run0.Arguments = filePath;
-            run0.UseShellExecute = false;
-            run0.RedirectStandardOutput = true;
-            run0.CreateNoWindow = Console;
-            Process = Process.Start(run0);
-            StreamReader reader = Process.StandardOutput;
-            return reader.ReadToEnd();
+            ProcessStartInfo PIS = new ProcessStartInfo();
+            PIS.FileName = "python3_13\\python.exe";
+            PIS.Arguments = filePath;
+            PIS.UseShellExecute = false;
+            PIS.RedirectStandardOutput = true;
+            PIS.CreateNoWindow = Console;
+            Process = Process.Start(PIS);
+            StreamReader sr = Process.StandardOutput;
+            return sr.ReadToEnd();
         }
     }
 }
